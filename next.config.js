fe0d5a5be52b5eb7954f26dd1d5bcd9a2afc5ac9
DongHyunYeom <sunderpower@naver.com>
2021-12-13 18:56:37 +0900

    base

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  images: {
+    domains: ['duckzill-resource.s3.ap-northeast-2.amazonaws.com'],
+  },
+  shortUrl:{
+    domains: ['https://openapi.naver.com/v1/util/shorturl'],
+  },
+  env: {
+    'MYSQL_HOST': '127.0.0.1',
+    'MYSQL_PORT': '3306',
+    'MYSQL_DATABASE': 'localPj',
+    'MYSQL_USER': 'root',
+    'MYSQL_PASSWORD': 'ejrwlf2021',
+    'NEXTAUTH_URL': 'http://localhost:3000/',
+    'KAKAO_API_KEY': 'c063be17296e1c7973d558319b0ebe5a',
+  },
 }
