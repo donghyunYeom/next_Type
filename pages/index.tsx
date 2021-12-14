5ae7df261df29daa787c8d0755faae20146a85cd
DongHyunYeom <sunderpower@naver.com>
2021-12-14 15:02:49 +0900

    csss

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,56 +2,65 @@ import type { NextPage } from 'next'
 import { ssApi } from './api/index'
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import {
-  HomeIcon,
-  SearchIcon,
-  UserIcon,
-  CubeIcon,
-  HeartIcon,
-  LinkIcon,
-  MailIcon
-} from "@heroicons/react/outline"
+import {useState, useEffect} from 'react';
 
-const Home: NextPage = () => {
+//const Home: NextPage = () => {
+const Homema = (props: any) => {
+
+  const [homema,setHome] = useState(props.detail);
+
   return (
+   
     <div className="hover:text-white ">
-            <header className="flex-1 flex-col sm:flex-row m-5 justify-between items-center mb-15 text-center">
-            <div className="flex-1 cursor-pointer transform hover:scale-105">
-                <LinkIcon className="flex-1 h-20 inline-block" />
-                <p className="flex-1 text-7xl font-extrabold inline-block" >Discord</p>
-            </div>
-            <div className="flex flex-grow justify-evenly max-w-sm">
-                {/* <HeaderItem title='HOME' Icon={HomeIcon} />
-                <HeaderItem title='MAIL' Icon={MailIcon} />
-                <HeaderItem title='SEARCH' Icon={SearchIcon} />
-                <HeaderItem title='ACCOUNT' Icon={UserIcon} /> */}
-            </div>
-          </header>
-
-            <div className="w-screen text-center mt-24 mb-24">
-                <div className="inline">
-                <input
-                    id="search_keyword"
-                    type="text"
-                    placeholder="search"
-                    className="w-2/3 h-12 text-center text-black"
-                />
-                <div className="inline-block bg-black text-white h-14 w-12 text-center align-middle" >검색</div>
-                </div>
-            </div>
-
-            <div className="w-auto md:flex mt-20" >
-            <div className="md:flex-shrink-0"></div>
-               
-            </div> 
+        <div className="inline-block m-5">GUIDE</div>
+        <div className="inline-block">|</div>
+        <div className="inline-block m-5">EVENT</div>
+        <header className="flex-1 flex-col sm:flex-row m-5 justify-between items-center mt-20 mb-15 text-center">
+        <div className="flex-1 cursor-pointer transform hover:scale-105">
+            {/* <LinkIcon className="flex-1 h-16 inline-block" /> */}
+            <p className="flex-1 text-4xl block">OUR FAN SITE CHANNEL</p>
+            <p className="flex-1 text-11xl font-extrabold inline-block -mt-12" >Discord</p>
         </div>
+      </header>
+
+        <div className="w-screen text-center mt-8 mb-24">
+            <div className="inline">
+            <div className="inline-block w-0 h-full"></div>
+            <div className="inline align-middle">
+            <input
+                id="search_keyword"
+                type="text"
+                placeholder="SEARCH"
+                className="w-1/2 h-16 text-center text-black"
+            />
+            </div>
+            <style jsx>{`
+              .line {
+                line-height: 60px;
+              }
+            `}</style>
+            <div className="inline-block bg-black text-white h-16 w-32 text-center align-middle line" >ENTER</div>
+            </div>
+        </div>
+
+        <div className="w-screen flex justify-center">
+          {homema.map((homema : any) => (
+          <div className="" key={homema.idx}>
+            <div className="item m-12">{homema.name}</div>
+            <div className="item m-12"></div>
+            <div className="item m-12"></div>
+          </div>
+          ))}
+        </div>
+    </div>
   )
 }
 
 export async function getServerSideProps(context:any) {
    
   let detail = await ssApi.GetList();
+
+  //console.log(detail)
 
   return {
     props: {
@@ -60,4 +69,4 @@ export async function getServerSideProps(context:any) {
   };
 }
 
-export default Home
+export default Homema
