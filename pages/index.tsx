1b684311c2e5c377723a44f8e7452ce28dce8bcc
DongHyunYeom <sunderpower@naver.com>
2021-12-16 18:49:49 +0900

    modal ing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,46 @@
-import type { NextPage } from 'next'
+//import type { NextPage } from 'next'
 import { ssApi } from './api/index'
 import Head from 'next/head'
 import Image from 'next/image'
 import {useState, useEffect} from 'react';
+import Modal from 'react-modal';
 
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    background: 'rgba(255, 255, 255)',
+    width: '300px',
+    height: '300px',
+    color: 'black',
+  },
+  overlay: {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: 'rgba(0, 0, 0, 0.6)',
+
+  }
+};
 
 //const Home: NextPage = () => {
 const Homema = (props: any) => {
 
   const [homema,setHome] = useState(props.detail);
-  //const [showModal, setShowModal] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+
+  const modalInfo = (idx:any) => {
+
+    setShowModal(true);
+  }
+
+  
 
   return (
    
@@ -58,16 +89,39 @@ const Homema = (props: any) => {
               {homema.description}
             </div>
             <div className="inline-block">
-            {/* <button onClick={() => setShowModal(true)}>초대하기</button> */}
-              
+              <button data-msg={homema.idx} onClick={() => modalInfo(homema.idx)}>초대하기</button>
             </div>
             <div className="inline-block"><a href={homema.discord}>입장하기</a></div>
           </div>
           ))}
+
+          <Modal key={homema.idx} id={homema.idx}
+            isOpen={showModal} onRequestClose={() => setShowModal(false)} ariaHideApp={false} style={customStyles} >
+              <div className="text-center">
+                <div className="item">
+                <div className="item">
+                    <img className="w-48 rounded-full" src={homema.imagePath} alt="test" />
+                  </div>
+                  <div className="item">
+                    {homema.name} • {homema.nameKo}
+                  </div>
+                  <div className="item">
+                    {homema.description}
+                  </div>
+                </div>
+              </div>
+              
+              <button onClick={() => setShowModal(false)}>close</button>
+          </Modal>
+          
+
+            
         </div>
     </div>
   )
 }
+
+//Modal.setAppElement('#root')
 
 
 export async function getServerSideProps(context:any) {
@@ -84,3 +138,7 @@ export async function getServerSideProps(context:any) {
 }
 
 export default Homema
+function componentWillMount() {
+  throw new Error('Function not implemented.');
+}
+
